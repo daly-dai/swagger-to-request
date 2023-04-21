450e8ac457022d50309238fa09a80d89555102fe
dsh <18094277434@163.com>
2023-04-21 16:39:02 +0800

    feat: 将请求方式改为request包

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,26 @@
 import { defineConfig } from "rollup";
-import babel from "rollup-plugin-babel";
+// 让 rollup 能够处理外部依赖
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+// 自动将外部类库声明为 externals
+import externals from "rollup-plugin-node-externals";
+// 打包ts文件
 import typescript from "rollup-plugin-typescript";
+// 生成 d.ts 文件
 import dts from "rollup-plugin-dts";
+// 打包产物清除调试代码
+import strip from "@rollup/plugin-strip";
 
 const plugins = [
-  babel(),
   typescript({
     sourceMap: false,
   }),
+  commonjs(),
   resolve(),
+  externals({
+    devDeps: false, // devDependencies 类型的依赖就不用加到 externals 了。
+  }),
+  strip(),
 ];
 export default defineConfig([
   {
@@ -32,13 +42,13 @@ export default defineConfig([
         format: "umd",
         // 外部引入的模块需要显式告知使用的三方模块的命名，结合下面的external使用
         globals: {
-          axios: "superagent",
+          request: "request",
         },
         // 注意如果是umd格式的bundle的话name属性是必须的，这时可以在script标签引入后window下会挂载该属性的变量来使用你的类库方法
         name: "swagger-to-request",
       },
     ],
-    external: ["superagent"],
+    external: ["request"],
     plugins,
   },
   {

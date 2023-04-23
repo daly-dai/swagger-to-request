95f36f4d276b9ec904f42657089c88eec9be11c2
dsh <18094277434@163.com>
2023-04-23 09:43:13 +0800

    feat: 修改接口请求方式为superAgent

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -42,20 +42,20 @@ export default defineConfig([
         format: "umd",
         // 外部引入的模块需要显式告知使用的三方模块的命名，结合下面的external使用
         globals: {
-          request: "request",
+          request: "superagent",
         },
         // 注意如果是umd格式的bundle的话name属性是必须的，这时可以在script标签引入后window下会挂载该属性的变量来使用你的类库方法
         name: "swagger-to-request",
       },
     ],
-    external: ["request"],
+    external: ["superagent"],
     plugins,
   },
   {
     input: "src/type.d.ts",
     output: [
       {
-        file: "dist/index.d.ts",
+        file: "dist/type.d.ts",
         format: "es",
         plugins: [],
       },
